5204e3834f3e0a6f917c5e1e761ccadf3054f0d0
Erhan VsCode <erhanselman80-vscode@gmail.com>
2026-06-09 17:49:12 +0300

    commit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,7 +10,7 @@ import { themes as prismThemes } from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'nTenis',
+  title: 'NTenis',
   tagline: 'Tenis sitesi',
   favicon: 'img/favicon.ico',
 
@@ -20,15 +20,15 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://ntenis.vercel.app/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'ntenis', // Usually your GitHub org/user name.
+  projectName: 'ntenis', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
@@ -83,7 +83,7 @@ const config = {
         respectPrefersColorScheme: true,
       },
       navbar: {
-        title: 'My Site',
+        title: 'NTenis',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
@@ -96,11 +96,11 @@ const config = {
             label: 'Tutorial',
           },
           { to: '/blog', label: 'Blog', position: 'left' },
-          {
-            href: 'https://github.com/facebook/docusaurus',
-            label: 'GitHub',
-            position: 'right',
-          },
+          /*  {
+             href: 'https://github.com/facebook/docusaurus',
+             label: 'GitHub',
+             position: 'right',
+           }, */
         ],
       },
       footer: {
