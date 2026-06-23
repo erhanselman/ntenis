4fdb41fff5927636cf2f6e571c6521378a4438b1
Erhan VsCode <erhanselman80-vscode@gmail.com>
2026-06-23 15:56:51 +0300

    logo guncel

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ import { themes as prismThemes } from 'prism-react-renderer';
 const config = {
   title: 'NTenis',
   tagline: 'Tenis sitesi',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/favicon_ntenis.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
